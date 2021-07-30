4c94ee1ee1b8ec9c75b57c67ba0c51420a2f2dfe
Jetlee Joy <jetleejoy.thokkombel@yahoo.com>
2021-07-30 12:13:45 +0530

    Modified the iterate fun and added parse class

--- matchingParanthesis.py
+++ matchingParanthesis.py
@@ -27,24 +27,56 @@ class Stack:
             item = self.stack.pop(self.tos)
             print(item ," is deleted from stack")
             self.tos -= 1
-
+    #to display the element at top of stack
     def peek(self):
         if self.stack == []:
             print("Empty Stack, ",end="")
             return self.tos
         else:
             return self.stack[self.tos]
-    
+    # to view the full stack
     def iterate(self):
         for i in self.stack:
-            if i == self.stack[self.tos]:
-                print(i," -- > Stack Top")
-            else:
-                print(i)
+            print(i)
+
+class Parser:
+    def __init__(self,inputString):
+        #create dictionary for parser
+        self.open = {
+             '{': 1,
+             '[': 2,
+             '(': 3
+            }   
+
+        self.close = {
+             '}': 1,
+             ']': 2,
+             ')': 3
+            }
+        self.input = inputString
+
+    #analyzing the brackets
+    def checkBracket(self):
+        cinput = self.input #generate a copy of string
+        cinput = list(cinput) #created a list out of string
+        tracker = 0
+        for char in cinput:
+            if char in self.open.keys():
+                for value in self.open.keys():
+                    if value == char:
+                        self.openB.push(self.open[char])
+            if char in self.close.keys():
+                for value in self.close.keys():
+                    if value == char:
+                        self.closeB.push(self.close[char])
+
+
+#identify the unmatched paranthesis, remove it from string
+input = "(((9372absocnde2902{947209{{343}}3}))"
+
 
 def main():
-    pass
-
+    P = Parser(input)
 if __name__ == "__main__":
     main()
     
